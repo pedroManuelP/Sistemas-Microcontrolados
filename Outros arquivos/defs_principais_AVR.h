9113632852832f61fc2bc1ccb1a497405cb09573
Pedro Manuel <112979257+pedroManuelP@users.noreply.github.com>
2025-05-22 22:22:11 -0300

    Add files via upload

---
+++ Outros arquivos/defs_principais_AVR.h
@@ -0,0 +1,15 @@
+#ifndef _DEFS_PRINCIPAIS_AVR_H
+#define _DEFS_PRINCIPAIS_AVR_H
+
+#define F_CPU 16000000UL //define clock do processador
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+#define set_bit(Y,bit_X) (Y|=(1<<bit_X)) // set num bit
+#define clr_bit(Y,bit_X) (Y&=~(1<<bit_X)) // clear num bit
+#define cpl_bit(Y,bit_X) (Y^=(1<<bit_X)) // inverte um bit
+#define tst_bit(Y,bit_X) (Y&(1<<bit_X)) // testa um bit(isola um bit)
+
+#endif
