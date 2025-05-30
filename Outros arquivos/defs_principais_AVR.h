ecb0a6d0263054caee132f7a46f4df8ba8cb2771
Pedro Manuel <112979257+pedroManuelP@users.noreply.github.com>
2025-05-30 00:14:31 -0300

    Update defs_principais_AVR.h

--- Outros arquivos/defs_principais_AVR.h
+++ Outros arquivos/defs_principais_AVR.h
@@ -9,17 +9,9 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-#ifndef set_bit(Y,bit_X)
 #define set_bit(Y,bit_X) (Y|=(1<<bit_X)) // set num bit
-#endif
-#ifndef clr_bit(Y,bit_X)
 #define clr_bit(Y,bit_X) (Y&=~(1<<bit_X)) // clear num bit
-#endif
-#ifndef cpl_bit(Y,bit_X)
 #define cpl_bit(Y,bit_X) (Y^=(1<<bit_X)) // inverte um bit
-#endif
-#ifndef tst_bit(Y,bit_X)
 #define tst_bit(Y,bit_X) (Y&(1<<bit_X)) // testa um bit(isola um bit)
-#endif
-  
+
 #endif
