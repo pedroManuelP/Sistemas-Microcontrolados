fbd4b609df748fa9a867181964cc2a8095df2c5f
Pedro Manuel <112979257+pedroManuelP@users.noreply.github.com>
2025-05-30 22:43:14 -0300

    Update lcd_AVR.h

--- Outros arquivos/lcd_AVR.h
+++ Outros arquivos/lcd_AVR.h
@@ -13,7 +13,11 @@
 #define LCD_DATA PORTD
 #endif
 
-#define pulso_enable() _delay_us(1); set_bit(LCD_DATA,EN); _delay_us(100); clr_bit(LCD_DATA,EN); _delay_us(45)
+#ifndef LCD_CTRL
+#define LCD_CTRL PORTD 
+#endif
+
+#define pulso_enable() _delay_us(1); set_bit(LCD_CTRL,EN); _delay_us(100); clr_bit(LCD_CTRL,EN); _delay_us(45)
 
 void lcd_cmd(unsigned char c, char cd);
 void lcd_write(char *c);
@@ -24,9 +28,9 @@ void lcd_cmd(unsigned char c, char cd){
 	LCD_DATA = (c & 0xF0) | (LCD_DATA & 0x0F);
 	
 	if(cd==0)
-	clr_bit(LCD_DATA,RS);
+	clr_bit(LCD_CTRL,RS);
 	else
-	set_bit(LCD_DATA,RS);
+	set_bit(LCD_CTRL,RS);
 	pulso_enable();
 	
 	if((cd==0) && (c < 4)) _delay_ms(2);
@@ -34,9 +38,9 @@ void lcd_cmd(unsigned char c, char cd){
 	LCD_DATA = ((c & 0x0F) << 4) | (LCD_DATA & 0x0F);
 	
 	if(cd==0)
-	clr_bit(LCD_DATA,RS);
+	clr_bit(LCD_CTRL,RS);
 	else
-	set_bit(LCD_DATA,RS);
+	set_bit(LCD_CTRL,RS);
 	pulso_enable();
 	
 	if((cd==0) && (c < 4)) _delay_ms(2);
@@ -49,38 +53,19 @@ void lcd_write(char *c)
 }
 
 void lcd_init(){
-	clr_bit(LCD_DATA,EN);
-	clr_bit(LCD_DATA,RS);
+	DDRD = 0xFC;
+	clr_bit(LCD_CTRL,EN);
+	clr_bit(LCD_CTRL,RS);
 	_delay_ms(50);
 	
-	LCD_DATA = (0x33 & 0xF0) | (LCD_DATA & 0x0F);
-	clr_bit(LCD_DATA,RS);
-	pulso_enable();
-	LCD_DATA = ((0x33 & 0x0F) << 4) | (LCD_DATA & 0x0F);
-	clr_bit(LCD_DATA,RS);
-	pulso_enable();
-	
-	LCD_DATA = (0x32 & 0xF0) | (LCD_DATA & 0x0F);
-	clr_bit(LCD_DATA,RS);
-	pulso_enable();
-	LCD_DATA = ((0x32 & 0x0F) << 4) | (LCD_DATA & 0x0F);
-	clr_bit(LCD_DATA,RS);
-	pulso_enable();
-	
-	// Instruções: Canal de 4 bits, 2 linhas, caracteres de 5x10 bits
-	LCD_DATA = (0x28 & 0xF0) | (LCD_DATA & 0x0F);
-	clr_bit(LCD_DATA,RS);
-	pulso_enable();
-	LCD_DATA = ((0x28 & 0x0F) << 4) | (LCD_DATA & 0x0F);
-	clr_bit(LCD_DATA,RS);
-	pulso_enable();
-	
-	_delay_ms(5);
-	pulso_enable();
+	lcd_cmd(0x03,0);
+	_delay_ms(10);
+	lcd_cmd(0x03,0);
 	_delay_us(200);
-	pulso_enable();
-	pulso_enable();
+	lcd_cmd(0x03,0);
+	lcd_cmd(0x02,0);
 	
+	lcd_cmd(0x28,0);// Instruções: Canal de 4 bits, 2 linhas, caracteres de 5x10 bits
 	lcd_cmd(0x08,0); //Display off, Cursor off, Blink off
 	lcd_cmd(0x01,0); //Clear display, DDRAM address in counter = 0
 	lcd_cmd(0x0C,0); //Display on, Cursor off, Blink off
