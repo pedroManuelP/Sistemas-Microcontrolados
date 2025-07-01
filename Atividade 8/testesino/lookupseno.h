acb081d12ed8aa4498bfb78b25e30d092935b7d0
helio04 <helio.ttl.04@gmail.com>
2025-07-01 14:28:30 -0300

    limpando diretorio

--- Atividade 8/testesino/lookupseno.h
+++
@@ -1,9 +0,0 @@
-#ifndef LOOKUPSENO_H
-#define LOOKUPSENO_H
-#include <stdint.h>
-#include <math.h>
-#define PI 3.14159265
-#define NUM_TAPS 360
-void sine_rad(uint8_t * buffer, uint8_t amplitude, float phase_increment);
-void generate_sine_wave(uint8_t *buffer, float amplitude_v, float offset_v, float frequency, float sample_rate);
-#endif
