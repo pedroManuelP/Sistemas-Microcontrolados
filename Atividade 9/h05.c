b07a69e72fec7316b3c0135258e6e3ac0d7ad325
Valdemar Neto <108936921+Valdemar-Neto@users.noreply.github.com>
2025-07-14 16:10:58 -0300

    Update h05.c

--- Atividade 9/h05.c
+++ Atividade 9/h05.c
@@ -1,102 +1,61 @@
 /*
- * CFile2.c
- *
- * Created: 11/07/2025 15:31:56
- *  Author: valld
- */ 
+ * hc05.c
+ * Driver para m√≥dulo Bluetooth HC-05 com FreeRTOS
+ * Autor: valld
+ * Atualizado: 11/07/2025
+ */
 
-#include "hc5.h"
-#include "FreeRTOS.h"
-#include "task.h"
-#include "queue.h"
-
-//Mapeando todos os enderecos dos registrados UART0
-
-#define UBRR0H (*(volatile unsigned char *) 0xC5)
-#define UBRROL (*(volatile unsigned char *) 0xC4)
-#define UCRSR0C (*(volatile unsigned char *) 0xC2)
-#define UCRSR0B (*(volatile unsigned char *) 0xC1)
-#define UCRSR0A (*(volatile unsigned char *) 0xC0)
-#define UDR0 (*(volatile unsigned char *) 0xC6)
-
-
-// setando os bits de controle
-
-#define TXEN0 3
-#define UDRE0 5
-#define UCSZ00 1
-#define UCSZ01 12
-
-// setando os tempos de clocks
+#include <avr/io.h>
+#include <string.h>
+#include "h05.h"
+#include "freeRTOS/FreeRTOS.h"
+#include "freeRTOS/queue.h"
+#include "freeRTOS/task.h"
 
 #define F_CPU 16000000UL
 #define BAUD 9600
-#define UBRR_VALUE ((F_CPU / (16UL * BAUD)) -1)
-
-// configuraÁ„o de controle
+#define UBRR_VALUE ((F_CPU / (16UL * BAUD)) - 1)
 
 #define TX_QUEUE_LEN 8
-#define TX_MAX_LEN 128
+#define TX_MAX_LEN   128
 
 static QueueHandle_t txQueue;
 
-// funcoes auxiliares uma vez que n„o pode utilziar string.h
+//inicializa o uart
+void hc05_init(void) {
+    UBRR0H = (unsigned char)(UBRR_VALUE >> 8);
+    UBRR0L = (unsigned char)(UBRR_VALUE);
 
-int str_len(const char *s) {
-	int i = 0;
-	while (s[i] != '\0'){
-		i++;
-	}
-	
-	return i;
+    UCSR0B = (1 << TXEN0) | (1 << RXEN0);   // Habilita TX e RX
+    UCSR0C = (1 << USBS0) | (3 << UCSZ00);  // 8 bits, 1 stop
+
+    txQueue = xQueueCreate(TX_QUEUE_LEN, TX_MAX_LEN);
 }
 
-void str_copy( char *dest, const char *src){
-	int i = 0;
-	while ((dest[i] = src[i]) != '\0'){
-		i++;
-	}
+// enviar uma string
+void hc05_send_string(const char *str) {
+    while (*str != '\0') {
+        while (!(UCSR0A & (1 << UDRE0)));  // Espera buffer livre
+        UDR0 = *str++;
+    }
 }
 
+// Envia string para a fila FreeRTOS (ass√≠ncrono)
+char hc05_send_async(const char *str) {
+    char buffer[TX_MAX_LEN];
+    if (strlen(str) >= TX_MAX_LEN) return 0;
 
-//inicializa usart
+    strcpy(buffer, str);
 
-void hc05_init(void){
-	UBRR0H = (unsigned char)(UBRR_VALUE >> 8);
-	UBRROL = (unsigned char)(UBRR_VALUE);
-	
-	UCRSR0B = (1 << TXEN0);//habilitando tx
-	UCRSR0C = (1 << UCSZ01) | (1 << UCSZ00); // habilitando 8bits
-	
-	txQueue = xQueueCreate(TX_QUEUE_LEN, TX_MAX_LEN);
+    return xQueueSend(txQueue, buffer, 0) == pdPASS;
 }
 
-// envia a string pela UART (bloqueado)
-void hc05_send_string(const char *str){
-	int i = 0;
-	while(str[i] != '\0'){
-		while(!(UCSR0A & (1 << UDRE0)));
-		UDR0 = str[i++];
-	}
-}
-
-//envia a string pela UART para envio assinrono 
-
-void hc05_send_async(const char *str){
-	char buffer[TX_MAX_LEN];
-	
-	if(str_len(str) >= TX_MAX_LEN) return 0;
-	
-	str_copy(buffer,str);
-	
-	if(xQueueSend(txQueue, &buffer, 0) == pdPass) return 1;
-	else return 0;
-}
-
-void vTaskBluetooth(void *pvParameters){
-	char msg[TX_MAX_LEN];
-	
-	while(1){
-		if (xQueueReceive(txQueue, &msg, portMAX_DELAY) == pdTRUE) hc05_send_string(msg);
-	}
+//Task do FreeRTOS que envia as strings da fila 
+void vTaskBluetooth(void *pvParameters) {
+    char msg[TX_MAX_LEN];
+    while (1) {
+        if (xQueueReceive(txQueue, &msg, portMAX_DELAY) == pdTRUE) {
+            hc05_send_string(msg);
+        }
+    }
 }
