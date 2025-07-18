4bb20d87d2c71dda9acda8a8ae085997c3b49e9d
Pedro Manuel <112979257+pedroManuelP@users.noreply.github.com>
2025-07-18 18:22:15 -0300

    Update h05.h

--- Atividade 9/h05.h
+++ Atividade 9/h05.h
@@ -12,5 +12,5 @@ void hc05_init(void); //inicializa o modulo com uart
 void hc05_send_string(const char *str);// envia uma string de forma bloqueante a uart
 char hc05_send_async(const char *str); // infileira uma string para o envio via bluetooth;
 char hc05_read_char(void);
-void hc05_load(void);
+void hc05_load(char *str);
 #endif
