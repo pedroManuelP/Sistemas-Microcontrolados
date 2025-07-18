a0d5b17a6a99b461de95815a542162c42fb16cba
Pedro Manuel <112979257+pedroManuelP@users.noreply.github.com>
2025-07-18 18:23:28 -0300

    Update main.c

--- Atividade 9/main.c
+++ Atividade 9/main.c
@@ -1,148 +1,81 @@
-/*
- * Project 09.c
- *
- * Created: 08/07/2025 17:05:51
- * Author : chamo
- */ 
-#include "defs_principais_AVR.h"
-#include "lcd.h"
-#include "twi_avr.h"
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "mpu6050.h"
+#include "header.h"
+#include "h05.h"
 
-#define S0 PC0
-#define S1 PC1
-#define S2 PC2
-#define S3 PC3
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
 
-#define SMPRT_DIV_addr 0x19
-#define GYRO_CONFIG_addr 0x1B
-#define	ACCEL_CONFIG_addr 0x1C
-#define PWR_MGMT_1addr 0x6B
+#define botaoS1 1
+#define botaoS2 2
+#define botaoS3 3
+#define botaoS0 0
 
-#define ACCEL_XOUT_H_addr 0x3B
-#define ACCEL_XOUT_L_addr 0x3C
-#define ACCEL_YOUT_H_addr 0x3D
-#define ACCEL_YOUT_L_addr 0x3E
-#define ACCEL_ZOUT_H_addr 0x3F
-#define ACCEL_ZOUT_L_addr 0x40
 
-#define TEMP_OUT_H_addr 0x41
-#define TEMP_OUT_L_addr 0x42
+ISR(PCINT1_vect);
+void setup();
+SemaphoreHandle_t xSemaphore;
+void setInterruptions();
+void loadDisplay();
+void readMPU();
 
-#define GYRO_XOUT_H_addr 0x43
-#define GYRO_XOUT_L_addr 0x44
-#define GYRO_YOUT_H_addr 0x45
-#define GYRO_YOUT_L_addr 0x46
-#define GYRO_ZOUT_H_addr 0x47
-#define GYRO_ZOUT_L_addr 0x48
-
-void mde();
-void numIntoString(char *str, int start_pos,uint16_t num);
-void mpu_init();
-uint8_t mpu_read(uint8_t addr);
-
-uint8_t state = 0;
-uint8_t updateLCD = 1;
-uint16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
-
-//Endereco I2C do MPU6050
-const int MPU=0x68;
-
-char bottom_line[17] = "                \0";
-int main(void)
-{
-	DDRC = 0x00;
-	
-	PCICR = 0x02;
-	PCMSK1 = 0x0E;
-	sei();
-	
+int16_t g_accel_int[3];
+int16_t g_gyro_int[3];
+float g_euler_int[3];
+float g_temperatura;
+float g_accel_float[3];
+uint8_t state;
+void setup(){
+	setInterruptions();
 	lcd_init();
-	lcd_goto(0,0);
-    while (1) 
-    {
-		mde();
-    }
+	MPU6050_init();
+	hc05_init();
+	xSemaphore = xSemaphoreCreateMutex();
+	state =1;
 }
 
-void mde(){
-	switch(state){
-		//----------ACELEROMETRO----------
-		case 0:
-		if(updateLCD){
-			lcd_clear();
-			lcd_print("x(g) y(g) z(g)  ");
-			lcd_goto(1,0);
-			numIntoString(bottom_line,3,AcX);
-			numIntoString(bottom_line,8,AcY);
-			numIntoString(bottom_line,13,AcZ);
-			lcd_print(bottom_line);
-			updateLCD = 0;
-		}
-		break;
-		//----------ACELEROMETRO----------
-		
-		//----------GIROSCOPIO----------
-		case 1:
-		if(updateLCD){
-			lcd_clear();
-			lcd_print("x(d) y(d) z(d)  ");
-			lcd_goto(1,0);
-			numIntoString(bottom_line,3,GyX);
-			numIntoString(bottom_line,8,GyY);
-			numIntoString(bottom_line,13,GyZ);
-			lcd_print(bottom_line);
-			updateLCD = 0;
-		}
-		break;
-		//----------GIROSCOPIO----------
-		
-		//----------TEMP_E_MODULO_DE_EULER----------
-		case 2:
-		if(updateLCD){
-			lcd_clear();
-			lcd_print("x    y    z     ");
-			lcd_goto(1,0);
-			/*
-			numIntoString();
-			numIntoString();
-			numIntoString();
-			*/
-			lcd_print("PENDENTE........");
-			updateLCD = 0;
-		}
-		break;
-		//----------TEMP_E_MODULO_DE_EULER----------
-		default:
-		break;
+void setInterruptions(){
+	PCICR = (1<<PCIE1);
+	PCMSK1 = (1<<PCINT8) | (1<<PCINT9) | (1<<PCINT10) | (1<<PCINT11) ;
+	sei();
+}
+
+int main(){
+	setup();
+	xTaskCreate(loadDisplay, "loadDisplay", 256, NULL, 0, NULL);
+	xTaskCreate(readMPU, "readMPU", 256, NULL, 0, NULL);
+	vTaskStartScheduler();
+	for(;;);
+	return 0;
+}
+
+void readMPU(){
+	for(;;){
+		MPU6050_READ_SCALED(g_accel_int, g_gyro_int, &g_temperatura, g_euler_int, g_accel_float);
+		vTaskDelay(50/portTICK_PERIOD_MS);
 	}
 }
 
-void numIntoString(char *str, int start_pos,uint16_t num) {
-	str[start_pos-3] = ' ';
-	str[start_pos-2] = ' ';
-	str[start_pos-1] = ' ';
-	str[start_pos] = ' ';
-
-	if(num >= 1000){
-		str[start_pos-3] = (num/1000)%10 + '0';
+void loadDisplay(){
+	for(;;){
+		updateDisplay(state, g_gyro_int, &g_temperatura, g_euler_int, g_accel_float);
+		vTaskDelay(400/portTICK_PERIOD_MS);
 	}
-	if(num >= 100){
-		str[start_pos-2] = (num/100)%10 + '0';
-	}
-	if(num >= 10){
-		str[start_pos-1] = (num/10)%10 + '0';
-	}
-	str[start_pos] = num%10 + '0';
-}
-
-void mpu_init(){
-
 }
 
 ISR(PCINT1_vect){
-	_delay_ms(10);//debounce
-	
-	if(tst_bit(PINC,PC1) == 0x00) state = 0; updateLCD = 1;
-	if(tst_bit(PINC,PC2) == 0x00) state = 1; updateLCD = 1;
-	if(tst_bit(PINC,PC3) == 0x00) state = 2; updateLCD = 1;
+	if(~PINC&(1<<botaoS1)){
+		state = 1;
+	}
+	else if(~PINC&(1<<botaoS2)) state =2;
+	else if(~PINC&(1<<botaoS3)) state =3;
+	else if(~PINC&(1<<botaoS0)) {
+		for(uint8_t i =0; i<3; i++){
+			g_euler_int[i] = 0;
+		}
+	}
 }
